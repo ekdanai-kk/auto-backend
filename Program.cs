846746f337be203c4f30badf5bd05a5eee2ebac7
Ekdanai Kummee (IT) <ekdkumme@deltaww.com>
2026-05-27 15:24:33 +0700

    init c# asp.net core 8.0 api

---
+++ Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5000);
+});
+
+var app = builder.Build();
+
+app.MapGet("/api/data", () => new 
+{
+    Status = "success",
+    Message = "Hello World! This is response from your automated C# ASP.NET Core 8.0 API.",
+    Timestamp = DateTime.UtcNow.ToString("o")
+});
+
+app.Run();
